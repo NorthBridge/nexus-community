112214b391a9696b6b61f38bb8bfeeced61bdb43
kdflint <kathy.flint@northbridgetech.org>
2021-02-21 12:29:50 -0600

    init auth api (for testing)

---
+++ nexus/api/auth/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+$cleanUsername = '';
+
+if (isset($_GET['username'])) {
+    $cleanUsername = $_GET['username'];
+}
+
+if (isset($_POST['username'])) {
+    $cleanUsername = $_POST['username'];
+}
+
+if ($cleanUsername == 'baduser') {
+		header('Content-Type: application/json');
+		echo json_encode(array('status' => 'ERROR'));
+} else {
+		header('Content-Type: application/json');
+		echo json_encode(array('status' => 'OK'));
+}
+
+
+?>
+
